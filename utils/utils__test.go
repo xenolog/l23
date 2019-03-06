eb030815323929c082818f79c0318ba673b841a1
Sergey Vasilenko <svasilenko@mirantis.com>
2019-03-06 20:46:14 +0300

    FIX ordering for native network primitives,
    i.e. network interfaces, that listed in the both sections: interfaces and transformations.
    wanted behavior:
      * If NIC listed only in the interfaces section -- such NICs will be processed first, by alphabetical order
      * if NIC listed in both cases -- order will be defined by place in the more specific transformations order.

--- utils/utils__test.go
+++ utils/utils__test.go
@@ -1,7 +1,7 @@
 package utils
 
 import (
-	// "reflect"
+	"reflect"
 	"testing"
 )
 
@@ -20,6 +20,22 @@ func TestIndexString__unsorted_double_positive(t *testing.T) {
 func TestIndexString__unsorted_negative(t *testing.T) {
 	aa := []string{"eth3", "eth2", "eth5", "eth1", "eth0"}
 	if IndexString(aa, "XXX") >= 0 {
+		t.Fail()
+	}
+}
+
+func TestReverseString(t *testing.T) {
+	aa := []string{"a1", "a2", "a3", "a4", "a5"}
+	rv := ReverseString(aa)
+	if !reflect.DeepEqual(rv, []string{"a5", "a4", "a3", "a2", "a1"}) {
+		t.Fail()
+	}
+}
+
+func TestPrepndString(t *testing.T) {
+	aa := []string{"eth1", "eth2eeee", "eth3", "eth5"}
+	rv := PrependString(aa, "eth0")
+	if !reflect.DeepEqual(rv, []string{"eth0", "eth1", "eth2eeee", "eth3", "eth5"}) {
 		t.Fail()
 	}
 }
