f28e22141507bb60e2c6024666be4abc6a90dd11
Sergey Vasilenko <svasilenko@mirantis.com>
2019-04-10 20:34:20 +0300

    test for VLAN into Bridge

--- u1804/u1804__test.go
+++ u1804/u1804__test.go
@@ -169,5 +169,86 @@ func Test__Just_Bridge(t *testing.T) {
 	// fmt.Printf("%v", actualSC)
 	td.CmpDeeply(t, actualSC, wantedSC, "ETH and Bridge properties are not equal")
 }
+func Test__Bridge_with_vlans(t *testing.T) {
+	wantedState := make(npstate.NPStates)
+	brName := "br1"
+	wantedState[brName] = &npstate.NPState{
+		Name:   brName,
+		Action: "bridge",
+		Online: true,
+		L3: npstate.L3State{
+			IPv4: []string{"10.10.10.131/25"},
+		},
+	}
+	for _, linkName := range []string{"eth1", "eth2", "eth3", "eth4", "eth5", "eth3.103"} {
+		wantedState[linkName] = &npstate.NPState{
+			Name:   linkName,
+			Action: "port",
+			Online: true,
+		}
+		if linkName == "eth3.103" {
+			wantedState[linkName].L2 = npstate.L2State{
+				Bridge:  brName,
+				Parent:  "eth3",
+				Vlan_id: 103,
+			}
+		} else if linkName != "eth2" && linkName != "eth4" {
+			wantedState[linkName].L2 = npstate.L2State{
+				Bridge: brName,
+			}
+		}
+
+	}
+
+	savedConfig := NewSavedConfig(nil)
+	savedConfig.SetWantedState(&wantedState)
+	savedConfig.Generate()
+	actualYaml := savedConfig.String()
+	actualSC := new(SavedConfig)
+	if err := yaml.Unmarshal([]byte(actualYaml), actualSC); err != nil {
+		t.Logf("Can't unmarshall the actual YAML: %s\n%s", err, actualYaml)
+		t.FailNow()
+	}
+	wantedYaml := `
+    version: 2
+    renderer: networkd
+    ethernets:
+      eth1:
+        dhcp4: false
+        dhcp6: false
+      eth2:
+        dhcp4: false
+        dhcp6: false
+      eth3:
+        dhcp4: false
+        dhcp6: false
+      eth4:
+        dhcp4: false
+        dhcp6: false
+      eth5:
+        dhcp4: false
+        dhcp6: false
+    vlans:
+      eth3.103:
+        id: 103
+        link: eth3
+        dhcp4: false
+        dhcp6: false
+    bridges:
+      br1:
+        interfaces: ["eth1","eth3","eth3.103","eth5"]
+        addresses:
+          - 10.10.10.131/25
+        dhcp4: false
+        dhcp6: false
+`
+	wantedSC := new(SavedConfig)
+	if err := yaml.Unmarshal([]byte(wantedYaml), wantedSC); err != nil {
+		t.Logf("Can't unmarshall the wanted YAML: %s\n%s", err, wantedYaml)
+		t.FailNow()
+	}
+	// fmt.Printf("%v", actualSC)
+	td.CmpDeeply(t, actualSC, wantedSC, "ETH and Bridge properties are not equal")
+}
 
 // -----------------------------------------------------------------------------
